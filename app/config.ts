28284cdce1e16fde33537c09c7fc97dbeabddccd
Tyler Halfpenny <tchalfpenny@gmail.com>
2025-05-14 12:38:06 -0400

    Update metadata and social links

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://www.tylerhalfpenny.com",
+  title: "Tyler Halfpenny",
+  name: "Tyler's Portfolio",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/TylerHalfpenny",
+  github: "https://github.com/tchalfpenny",
+  instagram: "https://www.instagram.com/tyler.halfpenny",
+  linkedin: "https://www.linkedin.com/in/tylerhalfpenny/",
+  email: "mailto:tchalfpenny@gmail.com",
 };
